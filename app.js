c406bc96e6a06ac99d67baf063e6cf8f3c240707
yuta-hamasaki <yh02052002@gmail.com>
2024-07-29 16:53:43 -0700

    Completed

--- app.js
+++ app.js
@@ -1,3 +1,33 @@
 $(function() {
-  // YOUR CODE HERE
+  $('#toggleButton').click(function(){
+    $('#myParagraph').toggle()
+  });
+  
+  $('#colorButton').on("click", function(){
+    $('#colorDiv').css("backgroundColor", "yellow");
+  });
+
+  $('#addClassButton').click(function(){
+    $('#classDiv').clone().insertAfter("#classDiv:last")
+  })
+  $('#removeClassButton').click(function(){
+    if($("div#classDiv").length > 1){
+      $('#classDiv').remove()
+    }
+  })
+
+  $("#fadeInButton").click(function(){
+    $("#fadeDiv").fadeIn()
+  })
+  $("#fadeOutButton").click(function(){
+    $("#fadeDiv").fadeOut()
+  })
+
+
+  $("#slideUpButton").click(function(){
+    $("#slideDiv").slideUp()
+  })
+  $("#slideDownButton").click(function(){
+    $("#slideDiv").slideDown()
+  })
 })
